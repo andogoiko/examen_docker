a9576740094cf66e5fe0164abb3aa72944945f05
andogoiko <andoitz.goico@gmail.com>
2022-01-26 14:16:14 +0100

    correccional

--- webserver.js
+++ webserver.js
@@ -1,19 +1,18 @@
-const http = require('http');
+const http = require("http");
 const os = require("os");
 
-const ifaceIP = '0.0.0.0'; // Specifies the IP address we want to listen to
+const ifaceIP = "0.0.0.0"; // Specifies the IP address we want to listen to
 const port = process.argv[2] || 3000;
-const api = process.env.API || 'http://localhost:4000'
+const api = process.env.API || "http://localhost:4000";
 
 const server = http.createServer((req, res) => {
+  var host = server.address().address;
+  var port = server.address().port;
+  var hostname = os.hostname();
+  var time = new Date();
+  time = time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds() + " " + time.getMilliseconds();
 
-    var host = server.address().address;
-    var port = server.address().port;
-    var hostname = os.hostname();
-    var time =  new Date();
-    time = time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds() + ' ' + time.getMilliseconds()
-
-    var html = `
+  var html = `
     <div>
     web: <strong>${hostname}</strong> ${time}<br>
     <button id='btn' title='API'>Call API</button><hr>
@@ -42,12 +41,12 @@ const server = http.createServer((req, res) => {
     }    
     </script>
     `;
-    res.setHeader('Content-Type', 'text/html');
-    res.statusCode = 200;
-    res.end(html)
+  res.setHeader("Content-Type", "text/html");
+  res.statusCode = 200;
+  res.end(html);
 });
 
 server.listen(port, ifaceIP, () => {
-    console.log(`WEB Server running at http://${ifaceIP}:${port}/`);
-    console.log(`WEB Server reading from ${api}/`);
+  console.log(`WEB Server running at http://${ifaceIP}:${port}/`);
+  console.log(`WEB Server reading from ${api}/`);
 });
